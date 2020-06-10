cd2a35c1158a3f160f4f6b836bef301f8ed6d65c
Andrew Faulkner <andfaulkner@gmail.com>
2020-06-10 02:04:19 -0400

    Split out getAllNASDAQStocks function; more docs; etc

--- script/algo-test-1.ts
+++ script/algo-test-1.ts
@@ -10,6 +10,8 @@ type ThenArg<T> = T extends PromiseLike<infer U> ? U : T;
 
 type QuestradeApi = ThenArg<ReturnType<typeof redeemToken>>['qtApi'];
 
+type AllStocks = ThenArg<ReturnType<QuestradeApi['search']['allStocks']>>;
+
 /*-------------------------------------------- CONFIG --------------------------------------------*/
 // TODO get from env vars
 // e.g. "G5pYM3IB7oYBaGuY_vttXA5fd8Auh0P40"
@@ -21,16 +23,39 @@ let qtApi: QuestradeApi;
 
 /*------------------------------------------- HELPERS --------------------------------------------*/
 /**
- * Return full list of stocks from NASDAQ (WARNING: long-running)
+ * Return stocks from NASDAQ
  */
 const getNASDAQStocks = async (offset: number) => await qtApi.search.allStocks('NASDAQ', offset);
 
-const getAllMarkets = () => {
+/**
+ * Get full list of stocks in NASDAQ.
+ *
+ * WARNING: long-running
+ */
+const getAllNASDAQStocks = async () => {
+    let nasdaqStocks: AllStocks = [];
+
+    let counter = 0;
+    let currentStocks = await getNASDAQStocks(counter);
+
+    while (currentStocks.length > 0 && counter <= numStocks) {
+        console.log(currentStocks);
+        nasdaqStocks = nasdaqStocks.concat(currentStocks);
+        counter += 20;
+        currentStocks = await getNASDAQStocks(counter);
+    }
+
+    return nasdaqStocks;
+};
+
+/**
+ * Return full list of markets
+ */
+const getAllMarkets = async () => {
     const allMarkets = await qtApi.market.getAllMarkets();
     console.log(`allMarkets:`, allMarkets);
     return allMarkets;
 };
-
 
 /*----------------------------------------- MAIN SCRIPT ------------------------------------------*/
 /**
@@ -44,26 +69,15 @@ const main = async () => {
 
     const allMarkets = getAllMarkets();
 
-    // Grab stocks list, but limit the number returned
-
-    let allStocks = [];
-
-    let counter = 0;
-    let currentStocks = await getNASDAQStocks(counter);
-
-    while (currentStocks.length > 0 && counter <= numStocks) {
-        console.log(currentStocks);
-        allStocks = allStocks.concat(currentStocks);
-        counter += 20;
-        currentStocks = await getNASDAQStocks(counter);
-    }
+    // Grab NASDAQ stocks list, but limit the number returned
+    const nasdaqStocks = getAllNASDAQStocks();
 
     // Write all returned stocks into a data file
-    writeFileSync(path.join(rootPath, 'data/all-stocks.json'), JSON.stringify(allStocks));
+    writeFileSync(path.join(rootPath, 'data/nasdaq-stocks.json'), JSON.stringify(nasdaqStocks));
 
     // Print all returned stocks to console
     console.log(`\n\n\n`);
-    console.log(`allStocks:`, allStocks);
+    console.log(`nasdaqStocks:`, nasdaqStocks);
 };
 
 /*--------------------------------------------- RUN ----------------------------------------------*/
