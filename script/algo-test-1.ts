26dc789267001f604562aa1ab4d9fcc7d55b3165
Andrew Faulkner <andfaulkner@gmail.com>
2020-06-10 01:49:51 -0400

    Run prettier (after module install)

--- script/algo-test-1.ts
+++ script/algo-test-1.ts
@@ -1,21 +1,21 @@
-import { redeemToken } from "questrade-ts";
-import { writeFileSync } from "fs";
-import path from "path";
+import {redeemToken} from 'questrade-ts';
+import {writeFileSync} from 'fs';
+import path from 'path';
 
-import { path as rootPath } from "app-root-path";
+import {path as rootPath} from 'app-root-path';
 
 // TODO get from env vars
 // e.g. "G5pYM3IB7oYBaGuY_vttXA5fd8Auh0P40"
-const token = "DUMMY_TOKEN";
+const token = 'DUMMY_TOKEN';
 
 type ThenArg<T> = T extends PromiseLike<infer U> ? U : T;
 
-type QuestradeApi = ThenArg<ReturnType<typeof redeemToken>>["qtApi"];
+type QuestradeApi = ThenArg<ReturnType<typeof redeemToken>>['qtApi'];
 
 let qtApi: QuestradeApi;
 
 const getAllStocks = async (offset: number) => {
-    return await qtApi.search.allStocks("", offset);
+    return await qtApi.search.allStocks('', offset);
 };
 
 const main = async () => {
@@ -39,10 +39,7 @@ const main = async () => {
         currentStocks = await getAllStocks(counter);
     }
 
-    writeFileSync(
-        path.join(rootPath, "data/all-stocks.json"),
-        JSON.stringify(allStocks)
-    );
+    writeFileSync(path.join(rootPath, 'data/all-stocks.json'), JSON.stringify(allStocks));
 
     console.log(`\n\n\n`);
     console.log(`allStocks:`, allStocks);
